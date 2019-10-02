b7b8b5e989b079e3ad5aa120a1569d97f00ad7a5
Maximeurvoy <maxime.ptitx@gmail.com>
2019-10-02 11:16:21 +0200

    first commit ! Yeah git status

---
+++ index.js
@@ -0,0 +1,6 @@
+const chalk = require('chalk');
+
+console.log(chalk.bgBlack.blue(`Celine`));
+console.log(chalk.bgRed.white(`Rachel`));
+console.log(chalk.bgWhite.red(`Florent`));
+console.log(chalk.bgBlue.black(`Jordan`));
